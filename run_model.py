89a588d0b056450497654f86a5241c77fe941948
Iruvanti Gautam Srinidhi <gautams818@gmail.com>
2023-05-27 16:22:38 -0500

    media pose commits

--- run_model.py
+++ run_model.py
@@ -3,6 +3,90 @@ import os
 from yolov5.detect import run
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import mediapipe as mp
+import numpy as np
+
+def calculate_angle(a, b, c):
+    """
+    Calculate the angle between three points in degrees.
+
+    Args:
+        a (tuple): Coordinates of point a (ax, ay).
+        b (tuple): Coordinates of point b (bx, by).
+        c (tuple): Coordinates of point c (cx, cy).
+
+    Returns:
+        float: Angle between the line segments ab and bc in degrees.
+    """
+    # Calculate the angle using arctan2 and convert it to degrees
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    # Normalize the angle to be within [0, 180] degrees
+    if angle > 180.0:
+        angle = 360.0 - angle
+
+    return angle
+
+def process_frame_media_pipe(frame, frame_path):
+    """
+    Process a frame using MediaPipe to detect pose landmarks and calculate angles.
+
+    Args:
+        frame (numpy.ndarray): Input frame in BGR format.
+        frame_path (str): Path to save the processed frame.
+
+    Returns:
+        numpy.ndarray: Processed frame with annotated angles and landmarks.
+    """
+
+    # Convert the frame to RGB
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
+    mp_drawing = mp.solutions.drawing_utils
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Process the frame with MediaPipe
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        results = pose.process(frame_rgb)
+
+        # Do something with the results
+        if results.pose_landmarks:
+            # Access pose landmarks and perform further analysis
+            landmarks = results.pose_landmarks.landmark
+
+            # Get landmark indices for elbow and knee
+            left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+            left_elbow = landmarks[mp_pose.PoseLandmark.LEFT_ELBOW]
+            left_wrist = landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
+            left_hip = landmarks[mp_pose.PoseLandmark.LEFT_HIP]
+            left_knee = landmarks[mp_pose.PoseLandmark.LEFT_KNEE]
+            left_ankle = landmarks[mp_pose.PoseLandmark.LEFT_ANKLE]
+
+            # Calculate elbow angle
+            elbow_angle = calculate_angle(
+                (left_shoulder.x, left_shoulder.y),
+                (left_elbow.x, left_elbow.y),
+                (left_wrist.x, left_wrist.y)
+            )
+
+            # Calculate knee angle
+            knee_angle = calculate_angle(
+                (left_hip.x, left_hip.y),
+                (left_knee.x, left_knee.y),
+                (left_ankle.x, left_ankle.y)
+            )
+
+            # Draw angles on the frame
+            cv2.putText(frame, f"Elbow Angle: {elbow_angle:.2f} deg", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(frame, f"Knee Angle: {knee_angle:.2f} deg", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        # Draw pose landmarks on the frame
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        cv2.imwrite(frame_path, frame)
+
+    return frame
 
 
 def convert_mp4_to_jpg(video_path, output_folder):
@@ -64,23 +148,32 @@ def generate_video(base_img, input_directory, output_video_path):
     cv2.destroyAllWindows()
     video.release()
 
-# CHANGE HERE
-input_video_path = "./input/new2.mp4"
-image_folder_path = "./input/input_images"
+if __name__ == '__main__':
 
-# Call the function to convert the MP4 file to JPG images
-image_paths = convert_mp4_to_jpg(input_video_path, image_folder_path)
+    # CHANGE HERE
+    input_video_path = "./input/new2.mp4"
+    image_folder_path = "./input/input_images"
 
+    # Call the function to convert the MP4 file to JPG images
+    image_paths = convert_mp4_to_jpg(input_video_path, image_folder_path)
 
-runs_folder = "./runs/"
-name = "run"
+    for filename in os.listdir(image_folder_path):
+        # Check if the file has an image extension (e.g., .jpg, .png)
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            # Read the image file using OpenCV
+            image_path = os.path.join(image_folder_path, filename)
+            image = cv2.imread(image_path)
+            process_frame_media_pipe(image,image_path)
 
-delete_folder_contents(runs_folder)
+    runs_folder = "./runs/"
+    name = "run"
 
-run(weights='./weights/basket_rim.pt', source='./input/input_images', conf_thres=0.1, project=runs_folder, name=name)
+    delete_folder_contents(runs_folder)
 
+    run(weights='./weights/basket_rim.pt', source='./input/input_images', conf_thres=0.1, project=runs_folder, name=name)
 
-delete_folder_contents(image_folder_path)
+    delete_folder_contents(image_folder_path)
 
-base_img = cv2.imread('./runs/run/frame_0000.jpg')
-generate_video(base_img, "./runs/run", "./output/new_output")
+    base_img = cv2.imread('./runs/run/frame_0000.jpg')
+
+    generate_video(base_img, "./runs/run", "./output/new_output")
